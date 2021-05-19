c87b2a578bca17db22d8d7917a489af37a96fb89
MikaVainio <koti.mika.vainio@gmail.com>
2021-05-19 10:54:50 +0300

    Uusi versio kv-tiedostototeutuksesta

---
+++ main2.py
@@ -0,0 +1,9 @@
+# WEIGHT MANAGEMENT APPLICATION MAIN 
+
+# Libraries and modules
+import kivy # The Kivy framework
+kivy.require('2.0.0') # Minimum version of the framework
+from kivy.app import App 
+from kivy.uix.gridlayout import GridLayout
+
+# Initializations and class definitions
