abcee69c720faaaa074450b8e6cf02dc0afe1a59
MikaVainio <koti.mika.vainio@gmail.com>
2021-05-19 14:08:31 +0300

    Kolmas versio käyttöliittymästä

--- main2.py
+++ main2.py
@@ -3,7 +3,30 @@
 # Libraries and modules
 import kivy # The Kivy framework
 kivy.require('2.0.0') # Minimum version of the framework
-from kivy.app import App 
-from kivy.uix.gridlayout import GridLayout
+from kivy.app import App # A parent class for the main window
+from kivy.uix.gridlayout import GridLayout # A parent class for the layout
 
 # Initializations and class definitions
+
+# The layout
+class Glayout(GridLayout):
+     
+    # Functional parts ie methods
+    def calculate_bmi(self): # Calculates Body Mass Index
+        pass
+
+    def calulate_fat(self): # Calculates the Fat percentage
+        pass
+
+# Create the app
+class WeightManagementApp(App):
+    def build(self):
+        return Glayout()
+        
+# Run the app
+if __name__ == "__main__":
+   # Create the application object 
+   weightManagementApp = WeightManagementApp()
+
+   # Run it continuously
+   weightManagementApp.run()
