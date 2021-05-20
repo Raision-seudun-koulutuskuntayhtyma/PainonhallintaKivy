8481cbeaa96308b2a1328b327b633d3877d9d59f
MikaVainio <koti.mika.vainio@gmail.com>
2021-05-20 11:58:39 +0300

    Tilanne torstai 20.5 klo 12.00

--- main2.py
+++ main2.py
@@ -13,11 +13,17 @@ class Glayout(GridLayout):
      
     # Functional parts ie methods
     def calculate_bmi(self): # Calculates Body Mass Index
-        pass
+        person_height = float(self.height_value.text) / 100
+        person_weight = float(self.weight_value.text) 
+        person_bmi = round(person_weight / person_height ** 2)
+        self.bmi_value.text = str(person_bmi)
 
     def calulate_fat(self): # Calculates the Fat percentage
-        pass
-
+        person_height = float(self.height_value.text)
+        person_weight = float(self.weight_value.text)
+        person_age = float(self.age_value.text)
+        person_sex = float(self.sex_value.text)
+        # TODO: person_fat_percentage
 # Create the app
 class WeightManagementApp(App):
     def build(self):
